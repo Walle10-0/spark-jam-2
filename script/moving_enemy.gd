62767804d3e81f7449240ec20e9a6ef6ff6735c8
Walle10-0 <lukemauro900@gmail.com>
2026-02-21 16:49:21 -0500

    who needs a timer?

--- script/moving_enemy.gd
+++ script/moving_enemy.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 	Nav_Agent.target_position = Target.global_position
 
 func _physics_process(delta: float) -> void:
+	Nav_Agent.target_position = Target.global_position
 	if Nav_Agent.is_navigation_finished():
 		return
 	
@@ -18,9 +19,3 @@ func _physics_process(delta: float) -> void:
 	velocity = current_agent_position.direction_to(next_path_position) * SPEED * delta
 	
 	move_and_slide()
-	
-
-func _on_timer_timeout() -> void:
-	if Nav_Agent.target_position != Target.global_position:
-		Nav_Agent.target_position = Target.global_position
-	$Timer.start()
