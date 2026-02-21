c00cf4ea2f9b0709dfe0ceec45428830a0599cea
Walle10-0 <lukemauro900@gmail.com>
2026-02-21 18:52:44 -0500

    missiles explode

--- script/cannon.gd
+++ script/cannon.gd
@@ -3,13 +3,28 @@ extends Node2D
 @export var missile: AnimatedSprite2D
 @export var launcher: AnimatedSprite2D
 @export var target: Node2D
+@export var missile_prefab: PackedScene = preload("res://assets/missile.tscn")
 
 @export var SIGHT = 120
+@export var RELOAD_TIME: float = 2.0
 
 var target_dir: float = 0
+var reload: float = RELOAD_TIME
 
 func _physics_process(delta: float) -> void:
 	if target != null:
 		if self.global_position.distance_to(target.global_position) < SIGHT:
-			target_dir = self.global_position.angle_to(target.global_position)
-	launcher.rotation += (target_dir - launcher.rotation) * delta * 10
+			target_dir = self.global_position.angle_to_point(target.global_position)
+			if reload > RELOAD_TIME:
+				var new_shot: Node2D = missile_prefab.instantiate()
+				self.get_parent().add_child(new_shot)
+				new_shot.rotation = launcher.rotation
+				new_shot.global_position = launcher.global_position
+				reload = 0
+	launcher.rotation = target_dir
+	missile.rotation = target_dir
+	reload += delta
+	if reload > RELOAD_TIME:
+		missile.visible = true
+	else:
+		missile.visible = false
---
+++ script/missile.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+@export var fuel: GPUParticles2D
+@export var explosion: GPUParticles2D
+@export var speed: float = 75
+@export var launch_speed: float = 50
+
+var launch: bool = true
+
+func _physics_process(delta: float) -> void:
+	if launch:
+		self.linear_velocity = Vector2.from_angle(self.rotation) * launch_speed
+		launch = false
+	self.linear_velocity += Vector2.from_angle(self.rotation) * speed * delta
+
+
+func _on_body_entered(body: Node) -> void:
+	explosion.reparent(self.get_parent())
+	explosion.emitting = true
+	self.queue_free()
