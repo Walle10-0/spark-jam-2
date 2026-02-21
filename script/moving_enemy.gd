50e9b4f64a971caee9a9a8b98d41346707038556
Walle10-0 <lukemauro900@gmail.com>
2026-02-21 16:56:37 -0500

    idk i did something ig

--- script/moving_enemy.gd
+++ script/moving_enemy.gd
@@ -4,7 +4,10 @@ extends CharacterBody2D
 
 @export var Target: Node = null
 
-@onready var Nav_Agent = $NavigationAgent2D
+@export var animatedSprite: AnimatedSprite2D
+@export var Nav_Agent: NavigationAgent2D
+
+var bot_state: String = "_hostile"
 
 func _ready() -> void:
 	Nav_Agent.target_position = Target.global_position
@@ -18,4 +21,22 @@ func _physics_process(delta: float) -> void:
 	var next_path_position = Nav_Agent.get_next_path_position()
 	velocity = current_agent_position.direction_to(next_path_position) * SPEED * delta
 	
+	update_animation(velocity)
+	
 	move_and_slide()
+
+func update_animation(direction: Vector2):
+	if direction.length() < 1:
+		animatedSprite.play("default" + bot_state)
+	else:
+		if abs(direction.x) > abs(direction.y):
+			animatedSprite.flip_h = direction.x < 0
+			animatedSprite.play("side" + bot_state)
+		else:
+			animatedSprite.flip_h = false
+			if direction.y > 0:
+				animatedSprite.play("front" + bot_state)
+			else:
+				animatedSprite.play("back" + bot_state)
+		
+		animatedSprite.speed_scale = direction.length() / 10
